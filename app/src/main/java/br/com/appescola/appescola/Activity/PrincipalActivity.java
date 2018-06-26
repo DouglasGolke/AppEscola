10b99505e9ec5f4b0a719ca833767ca92fe6e086
Douglas <douglas_golke@hotmail.com>
2018-06-25 23:08:58 -0300

    features

--- app/src/main/java/br/com/appescola/appescola/Activity/PrincipalActivity.java
+++ app/src/main/java/br/com/appescola/appescola/Activity/PrincipalActivity.java
@@ -1,11 +1,20 @@
 package br.com.appescola.appescola.Activity;
 
+import android.app.UiAutomation;
+import android.os.health.UidHealthStats;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.common.internal.Objects;
+import com.google.android.gms.common.util.UidVerifier;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -21,11 +30,14 @@ import br.com.appescola.appescola.R;
 
 public class PrincipalActivity extends AppCompatActivity {
 
+    private String email;
+    private FirebaseAuth mAuth;
     private ListView listDados;
     private DatabaseReference databasereference;
     private ArrayList<Professores> listProfessor;
     private ArrayAdapter<Professores> arrayAdapterProfessor;
     private ValueEventListener eventListener;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,18 +48,29 @@ public class PrincipalActivity extends AppCompatActivity {
         listProfessor = new ArrayList<>();
         arrayAdapterProfessor = new ProfessoresAdapter(this, listProfessor);
         listDados.setAdapter(arrayAdapterProfessor);
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        email = autenticacao.getCurrentUser().getEmail();
 
         databasereference = ConfiguracaoFirebase.getFirebase().child("professor");
+//        databasereference.child("professor").child(mAuth.getCurrentUser().getUid()).getDatabase();
 
         eventListener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 listProfessor.clear();
                 for(DataSnapshot objSnapShot:dataSnapshot.getChildren()){
+//                    if(dataSnapshot.getChildren().equals(mAuth.getUid())){
+//                    Professores p = objSnapShot.getValue(Professores.class);
+//                    listProfessor.add(p);}
+//                }
+//                arrayAdapterProfessor.notifyDataSetChanged();
+                if (Objects.equal(objSnapShot.child("email").getValue(), email)) {
                     Professores p = objSnapShot.getValue(Professores.class);
                     listProfessor.add(p);
                 }
-                arrayAdapterProfessor.notifyDataSetChanged();
+                    arrayAdapterProfessor.notifyDataSetChanged();
+            }
+
             }
 
             @Override
